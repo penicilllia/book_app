544d86e69ddedb3846a96680237c1477eb7179d0
penicilllia <penicilllia@gmail.com>
2024-06-28 13:09:06 +0300

    Header

--- dist/app.js
+++ dist/app.js
@@ -1106,6 +1106,47 @@
     onChange.target = proxy => proxy?.[TARGET] ?? proxy;
     onChange.unsubscribe = proxy => proxy?.[UNSUBSCRIBE] ?? proxy;
 
+    class DivComponent {
+        constructor() {
+            this.el = document.createElement('div');
+        }
+
+        render() {
+            this.el;
+        }
+    }
+
+    class Header extends DivComponent{
+        constructor(appState) {
+            super();
+            this.appState = appState;
+        }
+
+        render() {
+            this.el.innerHTML = '';
+            this.el.classList.add('header');
+            this.el.innerHTML = `
+            <div>
+                <img src="/static/logo.svg" alt="logo" />
+            </div>
+            <div class="menu">
+            <a class="menu__item" href="#">
+                <img src="/static/search-2.svg" alt="search icon">
+                Book search
+            </a>
+            <a class="menu__item" href="#favorites">
+                <img src="/static/favorites.svg" alt="favorites icon">
+                Favorites
+                <div class="menu__counter">
+                    ${this.appState.favorites.length}
+                </div>
+            </a>
+        </div>
+        `;
+            return this.el;
+        }
+    }
+
     class MainView extends AbstractView {
         state = {
             list: [],
@@ -1131,10 +1172,15 @@
 
         render() {
             const main = document.createElement('div');
-            main.innerHTML = `Books count: ${this.appState.favorites.length}`;
+            // main.innerHTML = `Books count: ${this.appState.favorites.length}`;
             this.app.innerHTML = '';
             this.app.append(main);
-            this.appState.favorites.push('Book!');
+            this.renderHeader();
+        }
+
+        renderHeader() {
+            const header = new Header(this.appState).render();
+            this.app.prepend(header);
         }
     }
 
--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,5 +1,6 @@
 import { AbstractView } from "../../common/view.js";
 import onChange from "on-change";
+import { Header } from "../../components/header/header.js";
 
 export class MainView extends AbstractView {
     state = {
@@ -26,9 +27,14 @@ export class MainView extends AbstractView {
 
     render() {
         const main = document.createElement('div');
-        main.innerHTML = `Books count: ${this.appState.favorites.length}`;
+        // main.innerHTML = `Books count: ${this.appState.favorites.length}`;
         this.app.innerHTML = '';
         this.app.append(main);
-        this.appState.favorites.push('Book!');
+        this.renderHeader();
+    }
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header);
     }
 }
